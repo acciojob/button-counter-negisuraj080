20f8f74756b7d0f0fcf22b1030161eb23b7b01d5
suraj negi <negisuraj080@gmail.com>
2024-08-22 09:41:58 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import Counter from "./Counter";
+
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-        <div>suraj</div>
+        <Counter />
     </div>
   )
 }
